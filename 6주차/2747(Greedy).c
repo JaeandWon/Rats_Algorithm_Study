3d21e916e35b31082b0a7be386ac6b785fd2ec31
JUNGWOO JANG <jw1010110@gmail.com>
2022-01-27 01:13:48 +0900

    Modified 2747

--- 6주차/2747(Greedy).c
+++ 6주차/2747(Greedy).c
@@ -3,19 +3,6 @@
 #define MAX_FIBO 50
 int value[MAX_FIBO] = { 1,1 };
 
-int main()
-{
-    int i = 0;
-
-
-    int n;
-    scanf("%d", &n);
-    printf("%d", Fibonacci(n));
-
-
-
-    return 0;
-}
 int Fibonacci(int n)
 {
     if (n < 1)
@@ -28,4 +15,18 @@ int Fibonacci(int n)
     }
     value[n - 1] = Fibonacci(n - 2) + Fibonacci(n - 1);
     return value[n - 1];
+}
+
+int main()
+{
+    int i = 0;
+
+
+    int n;
+    scanf("%d", &n);
+    printf("%d", Fibonacci(n));
+
+
+
+    return 0;
 }
